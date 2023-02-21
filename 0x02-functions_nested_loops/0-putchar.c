1989fa180cb9335b80654c8fdb8dbe519f73877b
CJ-Apam <chijikeapam@gmail.com>
2023-02-21 14:28:59 -0800

    ndo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * Description: It prints the word main.h, followed by a new line.
  * Return: 0.
  */
 int main(void)
 {
-	char word[9] = "main";
+	char word[9] = "main.h";
 	int i;
 
 	for (i = 0; i < 9; i++)
